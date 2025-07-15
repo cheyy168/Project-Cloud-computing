365776d5d43f1a5b3da7095c68788afa391ec3fd
Agent-Z <chey17042005@gmail.com>
2025-07-15 17:09:54 +0700

    update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -34,7 +34,7 @@ resource "aws_launch_template" "web" {
               systemctl start httpd
               systemctl enable httpd
               echo "<h1>Cyber Security Trends 2025
-              s3://cyber-trends-static-assets-dev-007536ca/index.html</h1>" > /var/www/html/index.html
+              https://cyber-trends-static-assets-dev-007536ca.s3.us-west-2.amazonaws.com/index.html</h1>" > /var/www/html/index.html
               EOF
   )
 
